4f1a434e142a3db139399d040d460da78a7c9159
Bruno-Santiagofonseca <contato.brunosantiagofoto@gmail.com>
2026-06-22 23:27:59 -0300

    feat: botao de atualizacao PWA, service worker com cache versionado e auto-update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'album-copa-2026-v1';
+const CACHE_NAME = 'album-copa-2026-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -8,8 +8,22 @@ const ASSETS = [
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
   );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('message', (e) => {
+  if (e.data && e.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
 
 self.addEventListener('fetch', (e) => {
